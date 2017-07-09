b166a495c417de59712062b1688df8987ecf8ce0
catt877619 <nguyenkhang.3h@gmail.com>
2017-07-09 23:12:38 +0700

    -
    -

---
+++ IVS_Trainning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IVS_Trainning.Startup))]
+namespace IVS_Trainning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
